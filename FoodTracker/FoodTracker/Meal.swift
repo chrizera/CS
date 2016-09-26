278cde8e564778b4d7051160bddbb9423f5dccbe
Christian Perrone <christianperrone@MacBook-Pro-de-SUPPORT-2.local>
2016-09-26 14:50:43 -0300

    criada a classe meal, com propriedades e init

---
+++ FoodTracker/FoodTracker/Meal.swift
@@ -0,0 +1,31 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Christian Perrone on 26/09/16.
+//  Copyright © 2016 Christian Perrone. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    
+    // MARK: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    //Inicializa as propriedades
+    init?(name: String, photo: UIImage?, rating: Int) {
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        //Condiçao para indicar que a inicializacao falhou
+        if (name.isEmpty || rating < 0){
+            return nil
+        }
+    }
+    
+    
+}
