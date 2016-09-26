5a91ad33daab96ca3f5dd7fbaf04c38743bbc5e5
Christian Perrone <christianperrone@MacBook-Pro-de-SUPPORT-2.local>
2016-09-26 16:17:46 -0300

    criada tableview cell para a classe meal. criado um custom table view cell. inseridos label, view e imageview

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Christian Perrone on 26/09/16.
+//  Copyright © 2016 Christian Perrone. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
