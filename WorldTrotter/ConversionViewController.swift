d0ac881210c14828625d8965aa323e8b1edb53c1
Christian Perrone <christianperrone@Christian-MacBook-Pro.local>
2016-10-04 20:29:20 -0300

    deleted the view controller, created a new view controller, linked the view controller on storyboard to it

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Christian Perrone on 04/10/16.
+//  Copyright © 2016 Christian Perrone. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+    
+}
