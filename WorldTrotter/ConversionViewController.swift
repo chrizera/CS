52a8cf5cf95f6f98f54d7c4eff1266ecdef53fc2
Christian Perrone <christianperrone@Christian-MacBook-Pro.local>
2016-10-04 21:07:41 -0300

    added 2 new vars and 1 function to make the conversion

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -13,6 +13,29 @@ class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
+    var fahrenheitValue: Double? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
+        }
+        else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = "\(value)"
+        }
+        else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
     @IBAction func fahrenheitFieldEditingChanged(textField: UITextField) {
         
         //celsiusLabel.text = textField.text
