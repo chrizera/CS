9d376c4ceb6c44bdce3ed5f795ef737407fdbe88
Christian Perrone <christianperrone@Christian-MacBook-Pro.local>
2016-10-10 17:40:45 -0300

    created the Item class and added the tableview controller to the storyboard

---
+++ Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Christian Perrone on 10/10/16.
+//  Copyright © 2016 Christian Perrone. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
