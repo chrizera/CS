47e271c41cc029c2382167b486a45187a7153ab5
Christian Perrone <christianperrone@Christian-MacBook-Pro.local>
2016-10-13 17:45:37 -0300

    added methods and a tap gesture recognizer to make the keyboard dismiss when return is pressed or when anywhere on screen is tapped

--- Homepwner/Homepwner/DetailViewController.swift
+++ Homepwner/Homepwner/DetailViewController.swift
@@ -8,13 +8,17 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UITextFieldDelegate {
     
     
     @IBOutlet var nameField: UITextField!
     @IBOutlet var serialNumberField: UITextField!
     @IBOutlet var valueField: UITextField!
     @IBOutlet var dateLabel: UILabel!
+    
+    @IBAction func backgroundTapped(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
     
     var item: Item!
     
@@ -51,6 +55,8 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
+        view.endEditing(true)
+        
         item.name = nameField.text ?? ""
         item.serialNumber = serialNumberField.text
         
@@ -60,5 +66,10 @@ class DetailViewController: UIViewController {
         else {
             item.valueInDollars = 0
         }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
